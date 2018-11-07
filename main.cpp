1831e02e8f07842c05e6e292abebeee753594160
Lars Muldjord <muldjordlars@gmail.com>
2018-11-07 22:04:05 +0100

    First simple cleaning implementation

--- main.cpp
+++ main.cpp
@@ -24,114 +24,94 @@
  */
 
 #include <iostream>
-#include <cstdio>
-#include <cstring>
+#include <vector>
+#include <climits>
 
 #include <sndfile.hh>
 
-#define BUFFER_LEN 1024
-
-static void
-create_file (const char * fname, int format)
-{	static short buffer [BUFFER_LEN] ;
-
-	SndfileHandle file ;
-	int channels = 1 ;
-	int srate = 44100 ;
-
-	printf ("Creating file named '%s'\n", fname) ;
-
-	file = SndfileHandle (fname, SFM_WRITE, format, channels, srate) ;
-
-	memset (buffer, 0, sizeof (buffer)) ;
-
-	file.write (buffer, BUFFER_LEN) ;
-
-	puts ("") ;
-	/*
-	**	The SndfileHandle object will automatically close the file and
-	**	release all allocated memory when the object goes out of scope.
-	**	This is the Resource Acquisition Is Initailization idom.
-	**	See : http://en.wikipedia.org/wiki/Resource_Acquisition_Is_Initialization
-	*/
-} /* create_file */
-
-static void
-read_file (const char * fname)
-{	short buffer[BUFFER_LEN] ;
-
-	SndfileHandle file ;
-
-	file = SndfileHandle(fname) ;
-
-	printf ("Opened file '%s'\n", fname) ;
-	printf ("    Sample rate : %d\n", file.samplerate ()) ;
-	printf ("    Channels    : %d\n", file.channels ()) ;
-
-	while(file.read(buffer, BUFFER_LEN) != 0) {
-	}
-
-	for(int a = 0; a < sizeof(buffer); ++a) {
-	  short prevBuf = (buffer[a - 1] >= 0?1:-1);
-	  short currBuf = (buffer[a] >= 0?1:-1);
-	  if(a > 0 && prevBuf != currBuf) {
-	    printf("\n");
-	  }
-	  printf("O");
-	}
-
-	/* RAII takes care of destroying SndfileHandle object. */
-} /* read_file */
-
 int main(int argc, char *argv[])
 {
-  if(argc != 2) {
+  printf("TapeWorm: A Memotech wav tapefile cleaner by Lars Muldjord, 2018\n");
+  std::string srcName;
+  std::string dstName = "output.wav";
+  if(argc == 2) {
+    srcName = argv[1];
+    dstName = "cleaned_" + srcName;
+  } else if(argc == 3) {
+    srcName = argv[1];
+    dstName = argv[2];
+  } else if(argc != 2) {
     printf("Usage: tapeworm SOURCE [DESTINATION]\n");
     return 0;
   }
-  const char * wavname = argv[1];
-  printf("Input: %s\n", wavname);
-  SndfileHandle wavfile(wavname);
-  printf ("  Samplerate : %d\n", wavfile.samplerate()) ;
-  printf ("  Channels   : %d\n", wavfile.channels()) ;
-  if(wavfile.samplerate() != 22050 && wavfile.samplerate() != 44100) {
-    printf("Error: File has samplerate of %d, tapeworm expects 22050 Hz or 44100 Hz, exiting...\n", wavfile.samplerate());
+  printf("Input: %s\n", srcName.c_str());
+  SndfileHandle srcFile(srcName.c_str());
+  printf("  Samplerate : %d\n", srcFile.samplerate()) ;
+  printf("  Channels   : %d\n", srcFile.channels()) ;
+  printf("  Format     : %#08x\n", srcFile.format());
+  if(srcFile.samplerate() != 22050 && srcFile.samplerate() != 44100) {
+    printf("Error: File has samplerate of %d, tapeworm expects 22050 Hz or 44100 Hz, exiting...\n", srcFile.samplerate());
     return 1;
   }
-  if(wavfile.channels() != 1) {
-    printf("Error: File contains %d channels, tapeworm expects 1, exiting...\n", wavfile.channels());
+  if(srcFile.channels() != 1) {
+    printf("Error: File contains %d channels, tapeworm expects 1, exiting...\n", srcFile.channels());
     return 1;
   }
 
-  short buffer[BUFFER_LEN] ;
+  short *buffer;
+  std::vector<short> *data = new std::vector<short>;
 
   // Read
-  while(file.read(buffer, BUFFER_LEN) != 0) {
+  while(srcFile.read(buffer, 1) != 0) {
+    data->push_back(*buffer);
   }
 
+  std::vector<short> *dataClean = new std::vector<short>;
   // Do stuff
-  	for(int a = 0; a < sizeof(buffer); ++a) {
-	  short prevBuf = (buffer[a - 1] >= 0?1:-1);
-	  short currBuf = (buffer[a] >= 0?1:-1);
-	  if(a > 0 && prevBuf != currBuf) {
-	    printf("\n");
-	  }
-	  printf("O");
-	}
-
+  {
+    unsigned long dots = 0;
+    unsigned long dotMod = data->size() / 10;
+    short thres = SHRT_MAX / 10;
+    printf("Cleaning and optimizing");
+    for(int a = 0; a < data->size(); ++a) {
+      if(data->at(a) > thres) {
+	dataClean->push_back(SHRT_MAX);
+      } else if(data->at(a) < -thres) {
+	dataClean->push_back(SHRT_MIN);
+      } else {
+	dataClean->push_back(data->at(a));
+      }
+	      
+      if(a % dotMod == 0) {
+	printf(".");
+	fflush(stdout);
+      }
+    }
+  }
+  
+  printf(" Done!\n");
 
   // Write
-  SndfileHandle file ;
-  int channels = 1 ;
-  int srate = 44100 ;
-  
-  printf ("Creating file named '%s'\n", fname) ;
-  
-  file = SndfileHandle (fname, SFM_WRITE, format, channels, srate) ;
-  
-  memset (buffer, 0, sizeof (buffer)) ;
-  
-  file.write (buffer, BUFFER_LEN) ;
+  {
+    unsigned long dots = 0;
+    unsigned long dotMod = dataClean->size() / 10;
+    printf("Saving to '%s'", dstName.c_str()) ;
+    SndfileHandle dstFile(dstName.c_str(), SFM_WRITE, srcFile.format(), srcFile.channels(), srcFile.samplerate());
+    for(int a = 0; a < dataClean->size(); ++a) {
+      if(a % dotMod == 0) {
+	printf(".");
+	fflush(stdout);
+      }
+      dstFile.write(&dataClean->at(a), 1);
+    }
+    
+    printf(" Done!\n");
+  }
+
+  delete data;
+  delete dataClean;
+
+  return 0;
 }
 
 
