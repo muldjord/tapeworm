192342cefb4d0869af9e33bdf8414767d8fff1db
Lars Muldjord <muldjordlars@gmail.com>
2018-11-11 23:50:41 +0100

    Just before switching to zero-cross algorithm

--- main.cpp
+++ main.cpp
@@ -53,6 +53,43 @@ int getZero(std::vector<short> &data, int idx, int span)
   return zero;
 }
 
+void pushInit(std::vector<short> &data, unsigned short &bitLengthOpt)
+{
+  for(int b = 0; b < ceil(bitLengthOpt / 2.0); ++b) {
+    data.push_back(SHRT_MIN);
+  }
+  for(int b = 0; b < bitLengthOpt + ceil(bitLengthOpt / 2.0); ++b) {
+    data.push_back(SHRT_MAX);
+  }
+}
+
+void pushStop(std::vector<short> &data, unsigned short &bitLengthOpt)
+{
+  for(int b = 0; b < bitLengthOpt * 2; ++b) {
+    data.push_back(SHRT_MIN);
+  }
+}
+
+void pushZero(std::vector<short> &data, unsigned short &bitLengthOpt)
+{
+  for(int b = 0; b < ceil(bitLengthOpt / 2.0); ++b) {
+    data.push_back(SHRT_MIN);
+  }
+  for(int b = 0; b < floor(bitLengthOpt / 2.0); ++b) {
+    data.push_back(SHRT_MAX);
+  }
+}
+
+void pushOne(std::vector<short> &data, unsigned short &bitLengthOpt)
+{
+  for(int b = 0; b < bitLengthOpt; ++b) {
+    data.push_back(SHRT_MIN);
+  }
+  for(int b = 0; b < bitLengthOpt; ++b) {
+    data.push_back(SHRT_MAX);
+  }
+}
+
 int main(int argc, char *argv[])
 {
   printf("TapeWorm: A Memotech wav tapefile cleaner and optimizer by Lars Muldjord 2018\n");
@@ -123,6 +160,31 @@ int main(int argc, char *argv[])
     printf(" Done!\n");
   }
 
+  // Write zero adjusted
+  {
+    printf("Saving to 'zero_adjusted.wav'");
+    SndfileHandle dstFile("zero_adjusted.wav", SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_U8, srcFile.channels(), srcFile.samplerate());
+    unsigned long dotMod = data.size() / BUFFER / 10;
+    short buffer[BUFFER] = {0};
+    dstFile.write(buffer, BUFFER); // Write a couple of silent buffers. Some players don't play
+    dstFile.write(buffer, BUFFER); // the first part, so a bit of silence is important
+    for(int a = 0; a < data.size(); a = a + BUFFER) {
+      sf_count_t write = 0;
+      for(int b = 0; b < BUFFER; ++b) {
+	if(a + b >= data.size())
+	  break;
+	buffer[b] = data.at(a + b);
+	write = b + 1;
+      }
+      dstFile.write(buffer, write);
+      if(a % dotMod == 0) {
+	printf(".");
+	fflush(stdout);
+      }
+    }
+    printf(" Done!\n");
+  }
+
   // Set maximum and minimum sample value
   int maxVal = 0;
   int minVal = 0;
@@ -145,7 +207,6 @@ int main(int argc, char *argv[])
     for(int a = 0; a < data.size(); ++a) {
       // Detect signal drop that indicate bit begin
       if(data.at(a) < minVal / INITDIV) {
-	printf("a: %d\n", a);
 	// Backtrack to bit beginning
 	while(a - 1 >= 0 && data.at(a - 1) < minVal / ZERODIV) {
 	  a--;
@@ -203,15 +264,17 @@ int main(int argc, char *argv[])
   {
     // Set bit length limits based on samplerate. This might need reworking
     unsigned short bitLengthOpt = srcFile.samplerate() / 2450;
-    unsigned short minLen = bitLength / 2;
-    unsigned short maxLen = (bitLength * 2) + (bitLength / 2);
+    unsigned short minLen = bitLength / 4;
+    unsigned short maxLen = bitLength * 3 + bitLength / 4;
     unsigned long dotMod = data.size() / 10;
     printf("  Detected zero bitlength = %d (appr. %d baud)\n", bitLength, srcFile.samplerate() / bitLength);
     printf("  Optimal zero bitlength = %d\n", bitLengthOpt);
     printf("  minLen=%d\n", minLen);
     printf("  maxLen=%d\n", maxLen);
     printf("Cleaning and optimizing");
+    bool inData = false;
     // Sample iteration
+    short allowedNullData = 0;
     for(int a = 0; a < data.size(); ++a) {
       // Detect signal drop that indicate bit begin
       if(data.at(a) < minVal / INITDIV) {
@@ -219,39 +282,59 @@ int main(int argc, char *argv[])
 	while(a - 1 >= 0 && data.at(a - 1) < minVal / ZERODIV) {
 	  a--;
 	}
-	long int curLength = 1;
-	while(a + curLength < data.size() && data.at(a + curLength) < maxVal / INITDIV) {
-	  curLength++;
+	long int loLen = 0;
+	long int hiLen = 0;
+	long int totLen = 0;
+	// Drop phase
+	while(a + totLen < data.size() && data.at(a + totLen) <= 0) {
+	  loLen++;
+	  totLen++;
 	}
-	while(a + curLength < data.size() && data.at(a + curLength) > maxVal / ZERODIV) {
-	  curLength++;
+	// Rise phase
+	while(a + totLen < data.size() && data.at(a + totLen) > maxVal / ZERODIV) {
+	  hiLen++;
+	  totLen++;
 	}
-	if(curLength <= minLen || curLength >= maxLen) {
-	  for(int b = 0; b < curLength; ++b) {
+	if(totLen <= minLen || totLen >= maxLen) {
+	  /*
+	  if(inData) {
+	    printf("STOPBIT!!!\n");
+	    // Push stop bit
+	    pushStop(dataClean, bitLengthOpt);
+	  }
+	  */
+	  inData = false;
+	  for(int b = 0; b < totLen; ++b) {
 	    dataClean.push_back(0);
 	  }
 	} else {
-	  printf("curLength=%ld\n", curLength);
-	  if(abs(curLength - bitLength) < abs(curLength - bitLength * 2)) {
-	    for(int b = 0; b < ceil(bitLengthOpt / 2.0); ++b) {
-	      dataClean.push_back(SHRT_MIN);
-	    }
-	    for(int b = 0; b < floor(bitLengthOpt / 2.0); ++b) {
-	      dataClean.push_back(SHRT_MAX);
+	  allowedNullData = bitLength;
+	  inData = true;
+	  if(abs(loLen - bitLength / 2) < abs(loLen - bitLength)) {
+	    if(abs(hiLen - bitLength / 2) < abs(hiLen - bitLength)) {
+	      // Push 0 bit
+	      pushZero(dataClean, bitLengthOpt);
+	    } else {
+	      // Push init bit
+	      pushInit(dataClean, bitLengthOpt);
 	    }
 	  } else {
-	    for(int b = 0; b < bitLengthOpt; ++b) {
-	      dataClean.push_back(SHRT_MIN);
-	    }
-	    for(int b = 0; b < bitLengthOpt; ++b) {
-	      dataClean.push_back(SHRT_MAX);
-	    }
+	    // Push 1 bit
+	    pushOne(dataClean, bitLengthOpt);
 	  }
-	    
 	}
-	a += curLength - 1;
+	a += totLen - 1;
       } else {
-	dataClean.push_back(0);
+	if(inData) {
+	  if(!--allowedNullData) {
+	    printf("STOPBIT!!!\n");
+	    // Push stop bit
+	    pushStop(dataClean, bitLengthOpt);
+	    inData = false;
+	  }
+	} else {
+	  dataClean.push_back(0);
+	}
       }
 	      
       if(a % dotMod == 0) {
@@ -263,7 +346,7 @@ int main(int argc, char *argv[])
   }
   // Write
   {
-    printf("Saving to '%s'", dstName.c_str()) ;
+    printf("Saving to '%s'", dstName.c_str());
     SndfileHandle dstFile(dstName.c_str(), SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_U8, srcFile.channels(), srcFile.samplerate());
     unsigned long dotMod = dataClean.size() / BUFFER / 10;
     short buffer[BUFFER] = {0};
